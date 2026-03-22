dbad8bf33b6f1fae5a317526beeddbb79d4b4af5
SohanDas04 <2330339@kiit.ac.in>
2026-03-22 22:25:48 +0530

    Project Completed

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId, //it basically tells mongodb that senderId/receiverId will be a reference to user model
+      ref: "User", //user model (this is being referrenced by senderId/receiverId)
+      required: "true",
+    },
+
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: "true",
+    },
+
+    text: {
+      type: String,
+    },
+
+    image: {
+      type: String,
+    }
+  },
+  { timestamps: true },
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages); //:id is a route parameter which is a dynamic placeholder
+
+router.post("/send/:id", protectRoute, sendMessage);
+
+export default router;
